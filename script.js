8be7a54877e1037d8b2666d60cb76d77d87b891e
HeyImJoella <joelle.iris@gmail.com>
2026-02-03 22:18:06 +0100

    nice

--- script.js
+++ script.js
@@ -12,14 +12,3 @@ document.querySelectorAll('input').forEach(input => {
     localStorage.setItem(key, input.value);
   });
 });
-
-// Optional: simple progressive overload for reps
-function checkRepsAndIncreaseKg(inputKg, inputReps) {
-  const reps = inputReps.value.split('x').map(x => parseInt(x));
-  const lastRep = reps[1];
-  if (lastRep >= 12 && inputKg.value) {
-    inputKg.value = parseFloat(inputKg.value) + 1;
-    localStorage.setItem(inputKg.dataset.key, inputKg.value);
-    alert(`Nice! ${inputKg.dataset.key} weight increased to ${inputKg.value}kg`);
-  }
-}
